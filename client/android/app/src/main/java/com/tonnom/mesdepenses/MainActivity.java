77295a48439467212f95c4ab3d5be04d337375c0
Mohamed <anzlatimohamed404@gmail.com>
2026-07-23 09:20:36 +0300

    Ajout icônes PWA pour publication sur les stores

---
+++ client/android/app/src/main/java/com/tonnom/mesdepenses/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tonnom.mesdepenses;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon.svg'],
+      includeAssets: ['icon.svg', 'apple-touch-icon-180x180.png', 'favicon.ico'],
       manifest: {
         name: 'Mes Dépenses',
         short_name: 'Mes Dépenses',
@@ -15,12 +15,34 @@ export default defineConfig({
         theme_color: '#0C447C',
         background_color: '#ffffff',
         display: 'standalone',
+        orientation: 'portrait',
         start_url: '/',
         icons: [
           {
             src: 'icon.svg',
             sizes: 'any',
             type: 'image/svg+xml'
+          },
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
